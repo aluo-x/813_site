0bfe28081ead94a2a4b7940c5b0acd3c67633ff8
Nikki Schmidt <nicole16@mit.edu>
2018-04-18 19:15:38 -0400

    add helper functions to mock data for easily adding new entities)

--- mock-data/people.js
+++ mock-data/people.js
@@ -67,8 +67,15 @@ const cat = {
   roles: [roles.ADMIN, roles.FOSTER, roles.CAGE_CLEANER],
 };
 
-const people_by_id = {
+var people_by_id = {
   0: raven,
   1: sam,
   2: cat,
+};
+
+const getNextPersonId = () => Math.max(...Object.keys(people_by_id)) + 1;
+
+const addPerson = (personProperties) => {
+  const id = getNextPersonId();
+  people_by_id[id] = Object.assign({}, personProperties, { id });
 };
