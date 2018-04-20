8a4534cfffa73cb5d78e00bfbfa237c2eded92ed
aluo-x <luo_andrew@outlook.com>
2018-04-19 21:54:37 -0400

    Quick mockup of animal modal

--- js/addEntity.js
+++ js/addEntity.js
@@ -1,39 +1,51 @@
-
 function openModal() {
-  document.getElementsByClassName('modal')[0].classList.add('is-active')
+    document.getElementsByClassName('modal')[0].classList.add('is-active')
 }
 
 function closeModal() {
-  document.getElementsByClassName('modal')[0].classList.remove('is-active');
+    document.getElementsByClassName('modal')[0].classList.remove('is-active');
 }
 
 function saveAnimal() {
-  // get properties from modal input
-  closeModal();
+    const animalName = document.getElementById('nameInput').value;
+    const adoptionStat = document.getElementById('adoptionStatusInput').value;
+    const animalGender = document.getElementById('genderInput').value;
+    const fixedStat = document.getElementById('fixedInput').value;
+    const animalSpecies = document.getElementById('speciesInput').value;
+    addAnimal({
+        name: animalName,
+        adoptionStatus: adoptionStat,
+        gender: animalGender,
+        fixed: fixedStat,
+        species: animalSpecies
+    });
+    refreshAnimalTable();
+    closeModal();
+
 }
 
 function saveEvent() {
-  // get properties from modal input
-  closeModal();
+    // get properties from modal input
+    closeModal();
 }
 
 function savePerson() {
-  const firstName = document.getElementById('firstNameInput').value;
-  const lastName = document.getElementById('lastNameInput').value;
-  const areaCode = document.getElementById('areaCodeInput').value;
-  const exchangeNumber = document.getElementById('exchangeNumberInput').value;
-  const endNumber = document.getElementById('endNumberInput').value;
-  const email = document.getElementById('emailInput').value;
+    const firstName = document.getElementById('firstNameInput').value;
+    const lastName = document.getElementById('lastNameInput').value;
+    const areaCode = document.getElementById('areaCodeInput').value;
+    const exchangeNumber = document.getElementById('exchangeNumberInput').value;
+    const endNumber = document.getElementById('endNumberInput').value;
+    const email = document.getElementById('emailInput').value;
 
-  const selectedRolesRaw = document.getElementById('rolesInput').selectedOptions;
-  const roles = Array.prototype.map.call(selectedRolesRaw, r => r.text);
-  addPerson({
-    firstName,
-    lastName,
-    phoneNumber: new Phone(areaCode, exchangeNumber, endNumber),
-    email: new Email(email),
-    roles,
-  });
-  refreshPeopleTable();
-  closeModal();
+    const selectedRolesRaw = document.getElementById('rolesInput').selectedOptions;
+    const roles = Array.prototype.map.call(selectedRolesRaw, r => r.text);
+    addPerson({
+        firstName,
+        lastName,
+        phoneNumber: new Phone(areaCode, exchangeNumber, endNumber),
+        email: new Email(email),
+        roles,
+    });
+    refreshPeopleTable();
+    closeModal();
 }
